8fb6fc6276222c73a121107f16bb44ce86e0fbec
LST-Hub <info@livestrongtechnologies.com>
2024-04-04 15:33:51 +0530

    Chnages in lead form

--- pages/ws/lead/index.js
+++ pages/ws/lead/index.js
@@ -29,9 +29,9 @@ const Leads = () => {
           buttonText={"Add Lead"}
           onButtonClick={handleButtonClick}
         />
-        <TkContainer>
+        {/* <TkContainer>
          <AllLead />
-        </TkContainer>
+        </TkContainer> */}
       </div>
     </>
   );
